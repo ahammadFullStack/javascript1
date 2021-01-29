5e6142bfc729b79f99bbf390a7fe2424293f93c5
ahammadFullStack <ahammad76718@gmail.com>
2021-01-29 11:26:25 +0530

    agga

---
+++ one.js
@@ -0,0 +1,9 @@
+var product_Name = "iphone 11";
+var price = 30000;
+var avilable = true;
+var store_Location;
+
+console.log(product_Name);
+console.log(price);
+console.log(avilable);
+console.log(store_Location);
